7145c120416fc3315c5ac2f82a3d92394277abac
Tarik Tabakovic <tariktab97@gmail.com>
2020-04-13 16:49:15 -0400

    implements incrementing in updateCount function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const counters = document.querySelectorAll('.counter');
 const speed = 200;
 
-counters.forEach(counter =>{
+counters.forEach(counter => { 
     const updateCount = () => {
-        const target = counter.getAttribute('data-target');
-        console.log(target);
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        
+        const inc = target / speed;
     }
     updateCount();
 });
